b69a9f9ff3ee5fabc3d42d5bd4efb9d9aa41c9bb
ataraxy-zhi <2711059631@qq.com>
2021-03-27 14:44:38 +0800

    backend init

---
+++ brief_notes_backend/src/test/java/com/study/briefnotes/BriefNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.briefnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
